3c49bfa5a949446b4d19b0dc43b2b83f3b71fc3a
Aristóteles Ferrão <afneto1960@gmail.com>
2025-01-16 16:57:51 -0300

    criação das classes e entidades

---
+++ src/main/java/com/SmStudies/assocEntitiesForOne/entities/Person.java
@@ -0,0 +1,69 @@
+package com.SmStudies.assocEntitiesForOne.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_person")
+public class Person {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Double salary;
+	
+	@ManyToOne
+	@JoinColumn(name = "department_id")
+	private Department department;
+	
+	public Person() {
+	}
+
+	public Person(Long id, String name, Double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Person id=" 
+				+ id 
+				+ ", name=" 
+				+ name 
+				+ ", salary=" 
+				+ salary;
+	}
+	
+
+}
