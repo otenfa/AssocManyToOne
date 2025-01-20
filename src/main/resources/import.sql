23a073f421269083085e57dad74631f676a0283f
Aristóteles Ferrão <afneto1960@gmail.com>
2025-01-20 11:54:21 -0300

    carga inicial do BD de teste

---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+--DEPARTMENTS
+INSERT INTO tb_department(NAME) values('Recusos Humanos');
+INSERT INTO tb_department(NAME) values('Financeiro');
+INSERT INTO tb_department(NAME) values('Serviços Gerais');
+
+
+--PERSONS
+INSERT INTO tb_person(NAME, SALARY, DEPARTMENT_ID) values('Algusto Thompson', 5000.00, 1);
+INSERT INTO tb_person(NAME, SALARY, DEPARTMENT_ID) values('Laura Craspel', 8000.00, 1);
+INSERT INTO tb_person(NAME, SALARY, DEPARTMENT_ID) values('Dipsee Leeren', 8000.00, 2);
+INSERT INTO tb_person(NAME, SALARY, DEPARTMENT_ID) values('Drausia Varela', 1500.00, 3);
